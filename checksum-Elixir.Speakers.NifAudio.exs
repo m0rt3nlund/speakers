22f7c76796a684261cdce67fa2e8f69926e3f5cc
morten.lund@maskon.no <morten.lund@maskon.no>
2026-07-31 11:54:08 +0200

    Update checksum-Elixir.Speakers.NifAudio.exs

--- checksum-Elixir.Speakers.NifAudio.exs
+++ checksum-Elixir.Speakers.NifAudio.exs
@@ -1,5 +1,5 @@
 %{
-  "libspeakers-v0.1.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b56836e6d80d5c2a9be00fd86a13f9e3f82e391d5aa1b4f386e9724521984905",
-  "speakers-v0.1.2-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:a4c0f6c961fc841e3c025f667dcc9d040dd911992faf06b40f22eede37121f9f",
-  "speakers-v0.1.2-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:0e3a74158d8f804a3a62ac6458635a09053c6ea363aaa4e5d02fc27b5ee24ba2",
+  "libspeakers-v0.1.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:02fd97ebbd70e89b19dc49f3086213864d0bdf47db193fbf0130dda7e81bfcd0",
+  "speakers-v0.1.2-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:910588b9b7b601836dc03b4bef97a3e0c7464ff931381c56c1c5aece296b2613",
+  "speakers-v0.1.2-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:ac75609b7c498c7328e13348e89970f39e954b796d8dcaaaaf3fc640cbd65166",
 }
